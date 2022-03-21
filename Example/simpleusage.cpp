22d3e90a93e38652c159f2deb5a163e149b39ec3
QIZI94 <qizi94@gmail.com>
2022-03-21 02:00:05 +0100

    Added wrirte and read interface functions requerement.
    Partially reverting major change to design in last comit, it is now responisiblity of interface instead of PropertyTemplate to define read behavior.
    Changing spaces to tabs.

--- Example/simpleusage.cpp
+++ Example/simpleusage.cpp
@@ -16,188 +16,186 @@ Property::Visitor::visit(visitor,{__VA_ARGS__});}
 
 
 void PropertyToString(const unip::Property& property, std::string& output){
-    using namespace unip;
-    Property::string_type propName = property.name();
-    output.append(propName);
+	using namespace unip;
+	Property::string_type propName = property.name();
+	output.append(propName);
 
-    if(property.isNameOnly()){
+	if(property.isNameOnly()){
 
-        output.append(": none");
-        return;
-    }
+		output.append(": none");
+		return;
+	}
 
-    Property::any_type value;
-    property.read(value);
+	Property::any_type value;
+	property.read(value);
 
-    if(Property::is_any<int>(value)){
-        output.append(": ");
-        output.append(std::to_string(Property::cast_any<int>(value)));
-    }
-    else if(Property::is_any<float>(value)){
-        output.append(": ");
-        output.append(std::to_string(Property::cast_any<float>(value)));
-    }
-    else if(Property::is_any<std::string_view>(value)){
-        output.append(": ");
-        output.append(Property::cast_any<const std::string_view&>(value));
-    }
+	if(Property::is_any<int>(value)){
+		output.append(": ");
+		output.append(std::to_string(Property::cast_any<int>(value)));
+	}
+	else if(Property::is_any<float>(value)){
+		output.append(": ");
+		output.append(std::to_string(Property::cast_any<float>(value)));
+	}
+	else if(Property::is_any<std::string_view>(value)){
+		output.append(": ");
+		output.append(Property::cast_any<const std::string_view&>(value));
+	}
 
-    output.append(", ");
+	output.append(", ");
 }
 
 bool SerializeFromString(const unip::Property& property, std::string_view& input){
-    using namespace unip;
-    auto itCurrent = input.begin();
-    auto itEnd = input.end();
+	using namespace unip;
+	auto itCurrent = input.begin();
+	auto itEnd = input.end();
 
+	if(itCurrent != itEnd){
+		auto itSpace = std::find(itCurrent, itEnd, ':');
 
-    //while(itCurrent != itEnd) {
-    if(itCurrent != itEnd){
-        auto itSpace = std::find(itCurrent, itEnd, ':');
+		if(itSpace == itEnd){
+			std::cerr<<"Wrong formating "<<input << '\n';
+		}
 
-        if(itSpace == itEnd){
-            std::cerr<<"Wrong formating "<<input << '\n';
-        }
+		std::string_view propName(&*itCurrent, std::distance(itCurrent,itSpace));
+		
+		if(propName != property.name()){
+			std::cerr<<"Wrong property name: "<<propName << " != " << property.name()<<'\n';
+			
+			return false;
+		}
+		itCurrent = itSpace + 2;
+		auto itComa = std::find(itCurrent, itEnd, ',');
 
-        std::string_view propName(&*itCurrent, std::distance(itCurrent,itSpace));
-        
-        if(propName != property.name()){
-            std::cerr<<"Wrong property name: "<<propName << " != " << property.name()<<'\n';
-            
-            return false;
-        }
-        itCurrent = itSpace + 2;
-        auto itComa = std::find(itCurrent, itEnd, ',');
+		std::string s_value(itCurrent, itComa);
 
-        std::string s_value(itCurrent, itComa);
+		unip::Property::any_type value;
+		property.read(value);
 
-        unip::Property::any_type value;
-        property.read(value);
+		if(Property::is_any<int>(value)){
+			
+			int i = std::stoi(s_value);
+			value = i;
+			property.write(value);
+		}
+		else if(Property::is_any<float>(value)){
+			float f = std::stof(s_value);
+			value = f;
+			property.write(value);
+		}
+	   else if(Property::is_any<std::string_view>(value)){
+			auto s_propVal = Property::cast_any<const std::string_view&>(value);
+			
+			if(s_propVal != s_value)
+			{
+				std::cerr<<"Wrong constant value: "<<s_propVal << " != " << s_value<<'\n';
+				return false;
+			}
+		}
+		
+		itCurrent = std::find_if(itComa, itEnd, [](char ch){return (ch != ' ' &&  ch != ',');});
+		input = std::string_view(itCurrent, std::distance(itCurrent, itEnd));
 
-        if(Property::is_any<int>(value)){
-            
-            int i = std::stoi(s_value);
-            value = i;
-            property.write(value);
-        }
-        else if(Property::is_any<float>(value)){
-            float f = std::stof(s_value);
-            value = f;
-            property.write(value);
-        }
-       else if(Property::is_any<std::string_view>(value)){
-            auto s_propVal = Property::cast_any<const std::string_view&>(value);
-            
-            if(s_propVal != s_value)
-            {
-                std::cerr<<"Wrong constant value: "<<s_propVal << " != " << s_value<<'\n';
-                return false;
-            }
-        }
-        
-        itCurrent = std::find_if(itComa, itEnd, [](char ch){return (ch != ' ' &&  ch != ',');});
-        input = std::string_view(itCurrent, std::distance(itCurrent, itEnd));
+		return true;
+	}
 
-        return true;
-    }
-
-    return false;
+	return false;
 }
 
 class Point{
-    public:
-    PROPERTIES(
-        Property("ClassName", std::string_view("Point")),
-        Property("x", x),
-        Property("y", y),
-    )
-    Point(){}
-    Point(int x, int y) : x(x), y(y){}
+	public:
+	PROPERTIES(
+		Property("ClassName", std::string_view("Point")),
+		Property("x", x),
+		Property("y", y),
+	)
+	Point(){}
+	Point(int x, int y) : x(x), y(y){}
 
-    void serialize(std::string& fileBuf, bool reading){
-        if(reading){
-            std::string_view entries = fileBuf;
-            unip::Property::Visitor serializationVisitor(
-                [&entries](const unip::Property& property){
-                    return SerializeFromString(property, entries);
-                }
-            );
-            propertiesFunc(serializationVisitor);
-        }
-        else{
-            fileBuf = toString();
-        }
+	void serialize(std::string& fileBuf, bool reading){
+		if(reading){
+			std::string_view entries = fileBuf;
+			unip::Property::Visitor serializationVisitor(
+				[&entries](const unip::Property& property){
+					return SerializeFromString(property, entries);
+				}
+			);
+			propertiesFunc(serializationVisitor);
+		}
+		else{
+			fileBuf = toString();
+		}
 
-    }
+	}
 
-    std::string toString() const{
-        std::string ret;
-        unip::Property::Visitor toStringVisitor(
-            [&ret](const unip::Property& property){
-                PropertyToString(property, ret);
-                return true;
-            }
-        );
-        propertiesFuncConst(toStringVisitor);
+	std::string toString() const{
+		std::string ret;
+		unip::Property::Visitor toStringVisitor(
+			[&ret](const unip::Property& property){
+				PropertyToString(property, ret);
+				return true;
+			}
+		);
+		propertiesFuncConst(toStringVisitor);
 
-        return ret;
-    }
+		return ret;
+	}
 
 
-    private:
-    int x=0,y=0;
-    
+	private:
+	int x=0,y=0;
+	
 };
 
 class Pointf{
-    public:
-    PROPERTIES(
-        Property("ClassName", std::string_view("Pointf")),
-        Property("x", x),
-        Property("y", y),
-    )
-    Pointf(){}
-    Pointf(float x, float y) : x(x), y(y) {}
+	public:
+	PROPERTIES(
+		Property("ClassName", std::string_view("Pointf")),
+		Property("x", x),
+		Property("y", y),
+	)
+	Pointf(){}
+	Pointf(float x, float y) : x(x), y(y) {}
 
-    void serialize(std::string& fileBuf, bool reading){
-        std::string_view entries = fileBuf;
-        unip::Property::Visitor serializationVisitor(
-            [&entries](const unip::Property& property){
-                return SerializeFromString(property, entries);
-            }
-        );
-        propertiesFunc(serializationVisitor);
-    }
+	void serialize(std::string& fileBuf, bool reading){
+		std::string_view entries = fileBuf;
+		unip::Property::Visitor serializationVisitor(
+			[&entries](const unip::Property& property){
+				return SerializeFromString(property, entries);
+			}
+		);
+		propertiesFunc(serializationVisitor);
+	}
 
-    std::string toString() const{
-        std::string ret;
-        unip::Property::Visitor toStringVisitor(
-            [&ret](const unip::Property& property){
-                PropertyToString(property, ret);
-                return true;
-            }
-        );
-        propertiesFuncConst(toStringVisitor);
+	std::string toString() const{
+		std::string ret;
+		unip::Property::Visitor toStringVisitor(
+			[&ret](const unip::Property& property){
+				PropertyToString(property, ret);
+				return true;
+			}
+		);
+		propertiesFuncConst(toStringVisitor);
 
-        return ret;
-    }
+		return ret;
+	}
 
 
-    private:
-    float x=0,y=0;
-    
+	private:
+	float x=0,y=0;
+	
 };
 
 
 
 int main(){
-    Point p;
-    Pointf pf;
-    std::cout<<p.toString()<<"\n";
-    std::cout<<pf.toString()<<"\n\n";
+	Point p;
+	Pointf pf;
+	std::cout<<p.toString()<<"\n";
+	std::cout<<pf.toString()<<"\n\n";
 
-    std::string s_pf = Pointf(5,4).toString();
-    pf.serialize(s_pf, true);
-    std::cout<<pf.toString()<<"\n";
+	std::string s_pf = Pointf(5,4).toString();
+	pf.serialize(s_pf, true);
+	std::cout<<pf.toString()<<"\n";
 
 }
--- property.hpp
+++ property.hpp
@@ -50,20 +50,20 @@ struct DefaultInterface{
 	using string_type_ref   = std::string_view;
 	template<typename T>
 	static any_type make_any(const T& value){
-        using type = std::remove_reference_t<T>;
+		using type = std::remove_reference_t<T>;
 		if constexpr(sizeof(type) > sizeof(void*)){
-            
+			
 			return std::make_any<const type*>(&value);
 		}
 		else{
 			return std::make_any<T>(value);
 		}
 	}
-    template<typename T>
+	template<typename T>
 	static any_type make_any(T& value){
-        using type = std::remove_reference_t<T>;
+		using type = std::remove_reference_t<T>;
 		if constexpr(sizeof(type) > sizeof(void*)){
-            
+			
 			return std::make_any<type*>(&value);
 		}
 		else{
@@ -73,19 +73,19 @@ struct DefaultInterface{
 
 	template<typename T>
 	static auto cast_any(const any_type& any){
-        using type = std::remove_reference_t<T>;
+		using type = std::remove_reference_t<T>;
 		if constexpr(sizeof(T) > sizeof(void*)){
-            return *std::any_cast<const type*>(any);
+			return *std::any_cast<const type*>(any);
 		}
 		else{
 			return std::any_cast<T>(any);
 		}
 	}
-    template<typename T>
+	template<typename T>
 	static auto cast_any(any_type& any){
-        using type = std::remove_reference_t<T>;
+		using type = std::remove_reference_t<T>;
 		if constexpr(sizeof(T) > sizeof(void*)){
-            return std::move(*std::any_cast<type*>(any));
+			return std::move(*std::any_cast<type*>(any));
 		}
 		else{
 			return std::any_cast<T>(any);
@@ -93,13 +93,25 @@ struct DefaultInterface{
 	}
 	template<typename T>
 	static bool is_any(const any_type& any){
-        using type = std::remove_reference_t<T>;
-        if constexpr(sizeof(type) > sizeof(void*)){
-            return (any.type() == typeid(const type*));
+		using type = std::remove_reference_t<T>;
+		if constexpr(sizeof(type) > sizeof(void*)){
+			return (any.type() == typeid(const type*));
 		}
 		else{
 			return (any.type() == typeid(type));
 		}
+	}
+	template<typename T>
+	static auto read(T& value){
+		return make_any<const T&>(value);
+	}
+	template<typename T>
+	static auto read(const T& value){
+		return make_any<T>(value);
+	}
+	template<typename T>
+	static auto write(any_type& any){
+		return cast_any<T>(any);
 	}
 
 	DefaultInterface() = delete;
@@ -119,10 +131,16 @@ template<class InterfaceImpl>
  *   * string_type_ref 	- type which will be used for passing the name of the named property by refference.
  * 
  * To satisfy function requirement following specification needs to be defined:
- * 	 * any_type make_any(type) 				- function which will take value and returns any_type.
+ * 	 ** Interface for user to use
+ *   * any_type make_any(type) 				- function which will take value and returns any_type.
+ * 	 * any_type make_any(const type) 		- function which will take const value and returns any_type.
  *   * type cast_any<type>(any_type&) 		- cast value contained by any_type to required type.
  *   * type cast_any<type>(const any_type&)	- cast value contained by any_type to required const type.
  *   * bool is_any<type>(const any_type&)	- checks if required type is same as one contained in any type
+ *   ** Interface for PropertyTemplate to use
+ *   * any_type read(type)                  - used by PropertyTemplate to read type value type into any type
+ *   * any_type read(const type)            - used by PropertyTemplate to read const type type value into any type
+ *   * type write(any_type&)                - used by PropertyTemplate to write any type value into type value
  *  Note: type or required type is meant as input type before its contained in any_type and when its returned from any_type
  **/
 class PropertyTemplate{
@@ -206,7 +224,7 @@ public: // static functions
 	static auto make_any(const T& value){
 		return interface::template make_any(value);
 	}
-    template<typename T>
+	template<typename T>
 	/**
 	 * Wraper function which wraps interface's implementation of make_any.
 	 * 
@@ -252,6 +270,39 @@ public: // static functions
 		return interface::template is_any<T>(any);
 	}
 
+	template<typename T>
+	/**
+	 * Wraper function which wraps interface's implementation of read.
+	 * 
+	 * @param value passes value by non-const reference.
+	 * @return value of interface implementation of read
+	 **/
+	static auto read(T& value){
+		return interface:: template read<T>(value);
+	}
+
+	template<typename T>
+	/**
+	 * Wraper function which wraps interface's implementation of read.
+	 * 
+	 * @param value passes value by const reference.
+	 * @return value of interface implementation of read
+	 **/
+	static auto read(const T& value){
+		return interface:: template read<T>(value);
+	}
+
+	template<typename T>
+	/**
+	 * Wraper function which wraps interface's implementation of write.
+	 * 
+	 * @param any passes any by any_type reference.
+	 * @return value of interface implementation of write
+	 **/
+	static auto write(any_type& any){
+		return interface:: template write<T>(any);
+	}
+
 	// helpers
 	template<class Callable, class... Args>
 	/**
@@ -261,11 +312,11 @@ public: // static functions
 	 * @param callable functor to be executed.
 	 * @param args arguments that will be passed to the functor.
 	 **/
-    static void ExecWhenNotConst(Callable callable, Args... args){
-        if constexpr(!detail::are_const<Args...>::value){
-            callable(std::forward<Args>(args) ...);
-        }
-    }
+	static void ExecWhenNotConst(Callable callable, Args... args){
+		if constexpr(!detail::are_const<Args...>::value){
+			callable(std::forward<Args>(args) ...);
+		}
+	}
 
 	template<typename T>
 	/**
@@ -287,7 +338,7 @@ public: // member functions
 	PropertyTemplate(string_type name, const T& constMember) : m_name(name),
 	m_read(
 		[&constMember](typename interface::any_type& entry){
-			entry = make_any<T>(constMember);
+			entry = read<T>(constMember);
 		}
 	), 
 	m_write({})
@@ -297,12 +348,12 @@ public: // member functions
 	PropertyTemplate(string_type name, T& member) : m_name(name), 
 	m_read(
 		[&member](typename interface::any_type& entry){
-			entry = make_any<const T&>(member);
+			entry = read<T>(member);
 		}
 	), 
 	m_write(
 		[&member](typename interface::any_type& entry){
-			member = cast_any<T>(entry);
+			member = write<T>(entry);
 		}
 	) 
 	{}
